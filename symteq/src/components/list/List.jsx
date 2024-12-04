671d2a1961557b791371493aff58bc3387ab5f56
martinacev <103221910+martinacev@users.noreply.github.com>
2024-12-04 11:13:14 +0100

    map

--- symteq/src/components/list/List.jsx
+++ symteq/src/components/list/List.jsx
@@ -2,28 +2,36 @@ import classes from "./List.module.css";
 import PropTypes from 'prop-types'; 
 import Button from "../cta-button/Cta-button";
 
-const List = ({name, number, amount}) => {
+const List = ({ items }) => {
     return (
-        
         <div className={classes.container}>
-              <Button label="Show List" size='medium' styleType="danger"></Button>
-             <p className={classes.items}>{name}</p>
-             <p className={classes.items}>{number}</p>
-             <p className={classes.items}>{amount}</p>
+            <Button label="Show List" size='medium' styleType="danger" />
+            {items.map((item, index) => (
+                <div key={index} className={classes.item}>
+                    <p className={classes.items}>{item.name}</p>
+                    <p className={classes.items}>{item.number}</p>
+                    <p className={classes.items}>{item.amount}</p>
+                </div>
+            ))}
         </div>
-    )
+    );
 }
 
 List.defaultProps = {
-    name: "Martin",
-    number: "072 268 283",
-    amount: "123412$"
+    items: [
+        { name: "Martin", number: "072 268 283", amount: "123412$" },
+        { name: "John", number: "078 145 678", amount: "54321$" }
+    ]
 };
 
 List.propTypes = {
-    name: PropTypes.string,
-    number: PropTypes.number,
-    amount: PropTypes.amount,
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired,
+            amount: PropTypes.string.isRequired
+        })
+    )
 };
 
 export default List;
